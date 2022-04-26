2dccd1f8f306983a6bce7c505acf25580e7c44ad
Ade-garr <ade-garr@student.42.fr>
2022-04-26 02:05:40 +0200

    event on numbers + erase

--- javascript.js
+++ javascript.js
@@ -1,26 +1,77 @@
-function updateScreen() {
+function updateScreen(number) {
+	display.textContent = number;
+}
 
+function addNumber(e) {
+	if (operator === undefined) { // add number to first member
+		if (first.length === 9) // don't add number if length > 9
+			return ;
+		else {
+			first += e.target.textContent;
+			updateScreen(first);
+		}
+	}
+	else { // add number to second member
+		if (second.length === 9) // don't add number if length > 9
+			return ;
+		else {
+			second += e.target.textContent;
+			updateScreen(second);
+		}
+	}
+}
+
+function eraseNumber() {
+	if (operator === undefined) { // erase number of first member
+		if (first.length === 0) // don't erase number if length = 0
+			return ;
+		else {
+			first = first.slice(0, -1);
+			updateScreen(first);
+		}
+	}
+	else { // erase number to second member
+		if (second.length === 0) // don't erase number if length = 0
+			return ;
+		else {
+			second = second.slice(0, -1);
+			updateScreen(second);
+		}
+	}
 }
 
 function reset() {
 	newNumber = true;
-	first = undefined;
-	second = undefined;
+	first = "";
+	second = "";
+	operator = undefined;
 	display.textContent = "0";
 }
 
 // get selectors for screen
 let display = document.querySelector('#display');
 let ac = document.querySelector('#ac');
+let numbers = document.querySelectorAll('.number');
+let erase = document.querySelector('.erase');
 
 // set event listener on clear button and inputs
 ac.addEventListener('click', reset);
+numbers.forEach(number => {
+	number.addEventListener('click', addNumber);
+});
+erase.addEventListener('click', eraseNumber);
 
 // variables initialization
-let newNumber = false;
-let first = undefined;
-let second = undefined;
+let newNumber = false; // à garder ?
+let first = "";
+let second = "";
+let operator = undefined;
 display.textContent = "4984";
 
 
 // reset();
+
+
+
+
+// regler le pb du plusieurs points dans mm nombre
